31d565de36d126beb620e1a0f745337dcbfd9b3d
Daniel McPherson <dmcpherson0@gmail.com>
2022-08-28 17:22:02 +1000

    Added scryfall card data download, and card image download

--- api_requests.py
+++ api_requests.py
@@ -2,19 +2,49 @@ import requests
 import json
 import datetime as dt
 
+
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-def loadcarddata(update=None):
-    # Load in scryfall carddata
+
+def loadcarddata(update=False):
+    # Load in scryfall carddata,
     filename = 'cardbulkdata.json'
-    if update == None:
-        with open(filename) as f:
-            scrydata = json.load(f)
-    print(scrydata)
+    if not update:
+        try:
+            with open(filename) as f:
+                scrydata = json.load(f)
+                print('Scryfall card data located')
+        except FileNotFoundError:
+            print('Scryfall card data not found, downloading now')
+            update = True
+    if update:
+        datadownload = requests.get(requests.get("https://api.scryfall.com/bulk-data").json()['data'][0]['download_uri'])
+        with open(filename, 'w') as f:
+            json.dump(datadownload.json(), f)
+        scrydata = datadownload.json()
     return scrydata
+
+
+def downloadcardimage(cardname, scryfallcarddata):
+    # Download and save card images from scryfall
+    for card in range(len(scryfallcarddata)):
+        if scryfallcarddata[card]['name'] == cardname:
+            carddata = scryfallcarddata[card]
+    art_types = carddata['image_uris'].keys()
+    for art_type in art_types:
+        imageurl = carddata['image_uris'][art_type]
+        cardimage = requests.get(imageurl)
+        file = open(cardname + '_' + art_type + '.png', "wb")
+        file.write(cardimage.content)
+        file.close()
+
+
+scryfalldata = loadcarddata()
+downloadcardimage('Narcomoeba', scryfalldata)
+
 
 # jprint(scrydata)
 
